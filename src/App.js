222b34f3ac087e06039edc14e7589c33e1641efa
karizmo <32525686+karizmo@users.noreply.github.com>
2020-01-02 16:27:52 -0800

    add preliminary notes and ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+//Create a ticket system
+//generate ticket per project to help track progress, proposal, and intention
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ticket System</h1>
+      <div>Department1</div>
+      <div>Department2</div>
+      <button>Generate Ticket</button>
     </div>
   );
 }
